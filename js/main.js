548c3707e937daaeae838aadf611792c6421d3f9
minhluc073 <nguyenminhluc0703@gmail.com>
2025-01-04 21:16:06 +0700

    updae

--- js/main.js
+++ js/main.js
@@ -42,6 +42,51 @@
     });
   };
 
+   /* otp input
+  ------------------------------------------------------------------------------------- */
+  var otpInput = function () {
+    if ($(".digit-group").length > 0) {
+      $(".digit-group")
+        .find("input")
+        .each(function () {
+          $(this).attr("maxlength", 1);
+          $(this).on("keyup", function (e) {
+            var valNum = $(this).val();
+            var parent = $($(this).parent());
+
+            if (e.keyCode === 8 || e.keyCode === 37) {
+              var prev = parent.find("input#" + $(this).data("previous"));
+
+              if (prev.length) {
+                $(prev).select();
+              }
+            } else if (
+              (e.keyCode >= 48 && e.keyCode <= 57) ||
+              (e.keyCode >= 65 && e.keyCode <= 90) ||
+              (e.keyCode >= 96 && e.keyCode <= 105) ||
+              e.keyCode === 39
+            ) {
+              var next = parent.find("input#" + $(this).data("next"));
+              if (!$.isNumeric(valNum)) {
+                $(this).val("");
+                return false;
+              }
+
+              if (next.length) {
+                $(next).select();
+              } else {
+                if (parent.data("autosubmit")) {
+                  parent.submit();
+                }
+              }
+            }
+          });
+        });
+    }
+  };
+
+
+
   /* delete Item 
   ------------------------------------------------------------------------------------- */
   var delItem = function () {
@@ -276,6 +321,7 @@
 
   $(function () {
     showPass();
+    otpInput();
     delItem();
     backPage();
     activeSuggest();
